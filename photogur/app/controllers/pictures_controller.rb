ac30d34322f494d6051c2918e51221bd818a6dbc
Jamil Velji <jamil@jamilvelji.com>
2014-08-27 13:47:40 -0400

    Moving @pictures in pictures_controller into database

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,43 +1,10 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = [
-      {
-        :title => "Never gonna give you up",
-        :artist => "doyouwatchsherlock",
-        :url => "http://i.imgur.com/FI2XOzv.png"
-        },
-        {
-        :title => "Well",
-        :artist => "cuboneder",
-        :url => "http://i.imgur.com/7ffU65Q.jpg"
-        },
-        {
-        :title => "Small sips guys",
-        :artist => "Anonymous",
-        :url => "http://i.imgur.com/it4kUmZ.jpg"
-        }
-    ]
+    @pictures = Picture.all
   end
 
   def show
-    @pictures = [
-      {
-        :title => "Never gonna give you up",
-        :artist => "doyouwatchsherlock",
-        :url => "http://i.imgur.com/FI2XOzv.png"
-        },
-        {
-        :title => "Well",
-        :artist => "cuboneder",
-        :url => "http://i.imgur.com/7ffU65Q.jpg"
-        },
-        {
-        :title => "Small sips guys",
-        :artist => "Anonymous",
-        :url => "http://i.imgur.com/it4kUmZ.jpg"
-        }
-    ]
-    @picture = @pictures[params[:id].to_i]
+    @picture = Picture.find(params[:id])
   end
 
   def new
--- photogur/db/seeds.rb
+++ photogur/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.create!(
+        :title => "Never gonna give you up",
+        :artist => "doyouwatchsherlock",
+        :url => "http://i.imgur.com/FI2XOzv.png"
+        )
+Picture.create(
+        :title => "Well",
+        :artist => "cuboneder",
+        :url => "http://i.imgur.com/7ffU65Q.jpg"
+        )
+Picture.create!(
+        :title => "Small sips guys",
+        :artist => "Anonymous",
+        :url => "http://i.imgur.com/it4kUmZ.jpg"
+        )
