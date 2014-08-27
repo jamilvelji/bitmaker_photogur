a9bd1348c54426e2d480dab61e095d956cbcf140
Jamil Velji <jamil@jamilvelji.com>
2014-08-27 13:23:50 -0400

    Making pictures show on /pictures

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,4 +1,21 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = [
+      {
+        :title => "Never gonna give you up",
+        :artist => "doyouwatchsherlock",
+        :url => "http://i.imgur.com/FI2XOzv.png"
+        },
+        {
+        :title => "Well... ",
+        :artist => "cuboneder",
+        :url => "http://i.imgur.com/7ffU65Q.jpg"
+        },
+        {
+        :title => "Small sips guys",
+        :artist => "Anonymous",
+        :url => "http://i.imgur.com/it4kUmZ.jpg"
+        }
+    ]
   end
 end
